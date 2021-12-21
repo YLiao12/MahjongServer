1cdecc6ec5df5e708b670264432bcca2c8797b78
YLiao12 <seu_lyq@outlook.com>
2021-12-21 07:35:10 +0000

    broadcast

--- Broadcast.py
+++ Broadcast.py
@@ -3,15 +3,19 @@ from flask_socketio import SocketIO, join_room, leave_room, send, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] =  'secret!'
-socketio =  SocketIO(app)
+socketio = SocketIO(app)
 
 @socketio.on('mj') 
 def my_event_1_handler(data):
     mj = data["mj"]
     room = data["room"]
     join_room(room)
-
     emit('mj', mj, room = room)
 
+@socketio.on('connect')
+def test_connect():
+    print('Client connected')
+    emit('my response', {'data': 'Connected'})
+
 if __name__ ==  '__main__':
-    socketio.run(app, host = '0.0.0.0', port='8085')
+    socketio.run(app, host = '0.0.0.0', port='8085', debug = True)
