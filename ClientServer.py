26266ef67d8832ee5c0fa966ea83f65fd60d2d1c
YLiao12 <seu_lyq@outlook.com>
2021-12-25 13:21:08 +0800

    player table change

--- ClientServer.py
+++ ClientServer.py
@@ -98,9 +98,11 @@ def coming_into_table():
             break
         except Exception:
             conn.ping(True)
+
     still_in_table = 1
-    insert_table_player = "insert into table_player (table_id, player_id, still_in_table) values (%s, %s, %s)"
-    insert_table_player_param = (table_id, player_id, still_in_table)
+    insert_table_player = "insert into %s (player_id) values (%s)"
+    insert_table_player_param = (redis_table_key, player_id)
+
     while True:
         try:
             cursor.execute(insert_table_player, insert_table_player_param)
@@ -141,11 +143,14 @@ def leaving_tables():
         except Exception:
             conn.ping(True)
     
-    update_table_player = "update table_player set still_in_table = 0 where table_id = %s and player_id = %s "
-    update_table_player_param = (table_id, player_id)
+    # update_table_player = "update table_player set still_in_table = 0 where table_id = %s and player_id = %s "
+    # update_table_player_param = (table_id, player_id)
+
+    delete_table_player = "delete from %s where player_id = %s"
+    delete_table_player_param = (redis_table_key, player_id)
     while True:
         try:
-            cursor.execute(update_table_player, update_table_player_param)
+            cursor.execute(delete_table_player, delete_table_player_param)
             conn.commit()
             break
         except Exception:
